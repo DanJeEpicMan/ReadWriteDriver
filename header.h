d526f3a679757a1c8108b6245e6a185b8be780f7
Daniel <Daniel@DESKTOP-2L6J1GV>
2023-07-11 17:19:28 -0400

    Add project files.

---
+++ header.h
@@ -0,0 +1,10 @@
+﻿NTSTATUS NTAPI MmCopyVirtualMemory
+(
+	PEPROCESS SourceProcess,
+	PVOID SourceAddress,
+	PEPROCESS TargetProcess,
+	PVOID TargetAddress,
+	SIZE_T BufferSize,
+	KPROCESSOR_MODE PreviousMode,
+	PSIZE_T ReturnSize
+);
